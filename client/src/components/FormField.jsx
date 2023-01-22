4c6003bc5720353b0c1d9413da9867c15c2230ba
Muhammad Iqbal <iqbal.adudu@gmail.com>
2023-01-22 02:18:43 +0200

    done fe

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -8,7 +8,7 @@ const FormField = ({
   value,
   handleChange,
   isSurpriseMe,
-  handleSupriseMe,
+  handleSurpriseMe,
 }) => {
   return (
     <div>
@@ -22,7 +22,7 @@ const FormField = ({
         {isSurpriseMe && (
           <button
             type="button"
-            onClick={handleSupriseMe}
+            onClick={handleSurpriseMe}
             className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
           >
             Surprise me
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,8 +16,14 @@ const CreatePost = () => {
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = () => {};
-  const handleChange = (e) => {};
-  const handleSurpriseMe = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+  const generateImg = () => {};
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -62,7 +68,34 @@ const CreatePost = () => {
                 className="w-9/12 h-9/12 object-contain opacity-40"
               />
             )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
           </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImg}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5"
+          >
+            {generatingImg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            Once you have created image you want, you can share it with others
+            in the community
+          </p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? "sharing..." : "share with the community"}
+          </button>
         </div>
       </form>
     </section>
